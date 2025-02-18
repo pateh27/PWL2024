49443bd17abb98cda8f817115717f07c3f2603f5
pateh27 <fathzfl@gmail.com>
2025-02-18 13:40:29 +0700

    added praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,63 +13,4 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/hello', function () {
-    return 'Hello World';
-});
-
-Route::get('/world', function () {
-    return 'World';
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/selamat', function () {
-    return('Selamat Datang');
-});
-
-Route::get('/about', function () {
-    return('2341720119, Muhammad Al-Fatih Ulima Robby');
-});
-
-Route::get('/user/{name}', function ($name) {
-    return('Nama saya '.$name);
-});
-
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});
-
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman artikel ke-'.$id;
-});
-
-Route:: get('/user/{name?}', function ($name='John') {
-    return 'Nama saya '.$name;
-});
-
-Route::get('/selamat', function () {
-    return('Selamat Datang');
-});
-
-Route::get('/about', function () {
-    return('2341720119, Muhammad Al-Fatih Ulima Robby');
-});
-
-Route::get('/user/{name}', function ($name) {
-    return('Nama saya '.$name);
-});
-
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});
-
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman artikel ke-'.$id;
-});
-
-Route:: get('/user/{name?}', function ($name='John') {
-    return 'Nama saya '.$name;
-});
-
+Route::get('/hello', [WelcomeController::class, 'hello']);
