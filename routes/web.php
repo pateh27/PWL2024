ae9c4bda43f94232c2afbccdb77f613e2527227c
pateh27 <fathzfl@gmail.com>
2025-02-18 15:55:33 +0700

    sebelum penambahan direktori blog

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
+
+Route::get('/greeting', function () {
+    return view('hello', ['name' => 'Fatih']);
+});
